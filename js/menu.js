b201f90f307bde75a0fb5a0d8095f390b2ae9941
georgecesar <giorgio.cesaroni@me.com>
2021-06-26 11:11:06 +0200

    Refined the contest section

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,12 @@
-let hamburger = document.querySelector('.hamburger');
-let menu = document.querySelector('#menu');
+let hamburger = document.querySelector(".hamburger");
+let menu = document.querySelector("#menu");
 
 hamburger.onclick = () => {
-  menu.classList.toggle('extended');
-
+  menu.classList.toggle("extended");
   document.body.classList.toggle("modal-open");
 };
 
 menu.onclick = () => {
-  menu.classList.remove('extended');
+  menu.classList.remove("extended");
   document.body.classList.toggle("modal-open");
 };
